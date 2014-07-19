3708da7a4cbfe47b0687d3c47a30a652b07366db
Kazuhiro Homma <kazu.homma@gmail.com>
2014-07-19 18:34:40 +0000

    力尽きた2014/07/20

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'CREATE INDEX'
-mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`, `created_at`);'
-mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_user_created_at` ON memos (`user`, `created_at`);'
+# mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`, `created_at`);'
+# mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_user_created_at` ON memos (`user`, `created_at`);'
 
