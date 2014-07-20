014ba5a7fbf6a9218515919cc955c705302609e5
Kazuhiro Homma <kazu.homma@gmail.com>
2014-07-20 10:28:24 +0000

    Revert "memを使い始めた"
    This reverts commit b6daef203c1660193beb58b1ae5e8e2426ee577b.
    
    Conflicts:
    	init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`, `created_at`);' >> /home/isucon/init.log 2>&1
-# mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_user_created_at` ON memos (`user`, `created_at`);' >> /home/isucon/init.log 2>&1
+echo 'CREATE INDEX'
+mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`, `created_at`);'
+mysql -uisucon isucon -e 'CREATE INDEX `memos_idx_user_created_at` ON memos (`user`, `created_at`);'
 # DELETE FROM public_memos WHERE id > 20540;
 # ALTER TABLE public_memos AUTO_INCREMENT = 20541;
 
-cd /home/isucon/webapp/ruby
-/home/isucon/env.sh bundle exec ruby init.rb >> /home/isucon/init.log 2>&1
